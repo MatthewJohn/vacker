b665da9bdebb6736eef08f782d7361a34dcd30c5
Matt <matthew@dockstudios.co.uk>
2017-10-15 06:49:57 +0100

    Update imported to use arg parser

--- bin/import_media.py
+++ bin/import_media.py
@@ -2,9 +2,19 @@
 
 import sys
 sys.path.append('.')
+import argparse
 
 from vacker.importer import Importer
 
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--directory', '-d', type=str, dest='directory',
+                    help='Directory to import', required=True)
+
+args = parser.parse_args()
+
+
+
 importer = Importer()
 # Need to obtain from arguments
-importer.import_directory('../sample_photos')
+importer.import_directory(args.directory)
