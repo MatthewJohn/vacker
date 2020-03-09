2d5384c7b7b7c071a8838036e760d5119e949843
Matt Comben <matthew@dockstudios.co.uk>
2020-03-09 19:33:22 +0000

    improve handler of duration pil_image is kept open

--- vacker/analyser/image.py
+++ vacker/analyser/image.py
@@ -26,40 +26,45 @@ class ImageAnalyser(GeolocationAnalyser):
         file_obj.properties['pv_date_taken'] = None
         file_obj.properties['pv_orientation'] = None
         gps_info = {}
+
+        raw_exif = None
         if hasattr(pil_image, '_getexif'):
-            exif_data = {
-                PIL.ExifTags.TAGS[k]: v
-                for k, v in pil_image._getexif().items()
-                if k in PIL.ExifTags.TAGS
-            } if pil_image._getexif() else None
-
-            if exif_data:
-                if 'Orientation' in exif_data:
-                    file_obj.properties['pv_orientation'] = exif_data['Orientation']
-
-                if 'GPSInfo' in exif_data:
-                    for key in exif_data['GPSInfo'].keys():
-                        decode = PIL.ExifTags.GPSTAGS.get(key,key)
-                        gps_info[decode] = exif_data['GPSInfo'][key]
-
-                    long_lat = cls._convert_gps_to_lang_lat(gps_info)
-
-                    if 'longitude' in long_lat and 'latitude' in long_lat:
-                        file_obj.properties['l_lat'] = float(long_lat['latitude'])
-                        file_obj.properties['l_lng'] = float(long_lat['longitude'])
-
-                for datetime_tag in ['DateTime',
-                                     'DateTimeOriginal',
-                                     'DateTimeDigitized']:
-                    if datetime_tag in exif_data:
-                        try:
-                            file_obj.properties['pv_date_taken'] = datetime.strptime(exif_data[datetime_tag], '%Y:%m:%d %H:%M:%S')
-                            break
-                        except:
-                            pass
-
+            raw_exif = pil_image._getexif()
         file_obj.properties['pv_width'], file_obj.properties['pv_height'] = pil_image.size
         pil_image.close()
+
+        exif_data = {
+            PIL.ExifTags.TAGS[k]: v
+            for k, v in raw_exif.items()
+            if k in PIL.ExifTags.TAGS
+        } if raw_exif else None
+
+        if exif_data:
+            if 'Orientation' in exif_data:
+                file_obj.properties['pv_orientation'] = exif_data['Orientation']
+
+            if 'GPSInfo' in exif_data:
+                for key in exif_data['GPSInfo'].keys():
+                    decode = PIL.ExifTags.GPSTAGS.get(key, key)
+                    gps_info[decode] = exif_data['GPSInfo'][key]
+
+                long_lat = cls._convert_gps_to_lang_lat(gps_info)
+
+                if 'longitude' in long_lat and 'latitude' in long_lat:
+                    file_obj.properties['l_lat'] = float(long_lat['latitude'])
+                    file_obj.properties['l_lng'] = float(long_lat['longitude'])
+
+            for datetime_tag in ['DateTime',
+                                 'DateTimeOriginal',
+                                 'DateTimeDigitized']:
+                if datetime_tag in exif_data:
+                    try:
+                        file_obj.properties['pv_date_taken'] = datetime.strptime(
+                            exif_data[datetime_tag],
+                            '%Y:%m:%d %H:%M:%S')
+                        break
+                    except Exception as exc:
+                        pass
 
         # If the datetime has been obtained from the meta information,
         # split the year, month and day out for easier searching/indexing
